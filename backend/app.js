0eff9ca33796a893787d02ff7490007a211e6938
alagunandhini <alagunadhini@gmail.com>
2025-07-02 14:11:59 +0530

    fix: added app.listen for Render deployment

--- backend/app.js
+++ backend/app.js
@@ -43,5 +43,11 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+const PORT = process.env.PORT || 9000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 
 module.exports = app;
